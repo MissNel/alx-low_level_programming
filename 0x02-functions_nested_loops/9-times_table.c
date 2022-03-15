b48341a2060da1c570ef6ddc3f3d33131f5026cc
root <maxineleenel@gmail.com>
2022-03-15 14:10:30 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,10 +27,10 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar((k / 10) + '0');
-				_putchar((k % 10) +'0');
+				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
 	}
 
-}
+
